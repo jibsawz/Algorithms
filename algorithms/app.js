2060b0efa63b48bae5f10db176702dacb7eb2133
rezaslm <rezaslm2002@gmail.com>
2025-01-10 21:24:28 +0330

    feat: selection algorithm

---
+++ algorithms/app.js
@@ -0,0 +1,15 @@
+function selectionSort(arr) {
+    const n = arr.length;
+  
+    for (let i = 0; i < n - 1; i++) {
+      let minIndex = i;
+      for (let j = i + 1; j < n; j++) {
+        if (arr[j] < arr[minIndex]) {
+          minIndex = j;
+        }
+      }
+  
+      // Smallest converter with current element
+      [arr[minIndex], arr[i]] = [arr[i], arr[minIndex]];
+    }
+  }
