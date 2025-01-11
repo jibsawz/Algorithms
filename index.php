76df3e6044d071a495cee02ec18d0449fcca4e4e
Danial <danialbarbodjalali@gmail.com>
2025-01-11 21:45:31 +0330

    feat : changing the format to php for headers

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- links -->
- 
+
   <link rel="stylesheet"
     href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
@@ -18,3 +18,17 @@
 </body>
 
 </html>
+
+<?php
+session_start();
+// Checking if user is logged in 
+if (isset($_SESSION['user_id'])) {
+  // if its logged in:
+  header("Location: pages/profile.php");
+  exit();
+} else {
+  // if its not logged in:
+  header("Location: pages/login.html");
+  exit();
+}
+?>
